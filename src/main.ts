1491effde2b2b50d801e5a7189ea50411650e80c
IsaX01 <isaiaybadia@gmail.com>
2024-11-24 21:19:28 -0400

    main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://isax01:isax@localhost:5672'],
+      queue: 'items_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
